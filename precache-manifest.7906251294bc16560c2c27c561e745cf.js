aeb260305ff653ad8f2e7e5921cf1be87ae98171
Evan <memorysd2013@gmail.com>
2019-10-14 11:42:30 +0800

    first commit

---
+++ precache-manifest.7906251294bc16560c2c27c561e745cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather-app/robots.txt"
+  },
+  {
+    "revision": "d83d49081303d7274083",
+    "url": "/weather-app/js/chunk-vendors.0284c43b.js"
+  },
+  {
+    "revision": "44b68781cdf671139590",
+    "url": "/weather-app/js/app.f64a6bbd.js"
+  },
+  {
+    "revision": "79e208c3a448af5a7c01",
+    "url": "/weather-app/js/about.1244f2ac.js"
+  },
+  {
+    "revision": "c1e4c4ee10603f853bac602b46b4b8d8",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "44b68781cdf671139590",
+    "url": "/weather-app/css/app.3f377301.css"
+  }
+];
